47a561d50ba3e8956ec9773a44951a61d663516e
unknown <discomushroom617@yahoo.com>
2019-01-10 17:54:51 -0600

    about to start

--- hangman/game.py
+++ hangman/game.py
@@ -1,4 +1,5 @@
-from .exceptions import *
+# from .exceptions import *
+import random
 
 
 class GuessAttempt(object):
@@ -10,4 +11,16 @@ class GuessWord(object):
 
 
 class HangmanGame(object):
-    pass
+    def __init__(self, word_list=['rmotr', 'python', 'awesome'], number_of_guesses=5):
+        self.WORD_LIST = word_list
+        self.number_of_guesses = number_of_guesses
+
+    def select_random_word(self, list_of_words):
+        return random.choice(list_of_words)
+
+
+a = HangmanGame()
+# print(a.WORD_LIST)
+print(a.WORD_LIST)
+# print(a.number_of_guesses)
+print(a.select_random_word(['hi', 'there', 'how', '1', '2',3,4,5,6,7,8,9,0]))
